97089a1b5fc814a2f0223c3c4254663d3333cc7e
Isaacg1 <iag8@njit.edu>
2023-04-19 14:17:03 -0400

    calulator

--- index.js
+++ index.js
@@ -1,99 +1,46 @@
-const display = document.querySelector('#display');
-const buttons = document.querySelectorAll('.buttons');
+const buttons = document.querySelectorAll(".buttons");
+const display = document.querySelector("#display");
 
-let currentNumber = '';
-let firstNumber = null;
+let operand1 = null;
 let operator = null;
-let result = null;
+let operand2 = null;
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const buttonValue = button.textContent;
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const buttonValue = event.target.innerText;
+    const isNumber = !isNaN(buttonValue);
     
-    // Check for clear button
-    if (buttonValue === 'C') {
-      currentNumber = '';
-      firstNumber = null;
-      operator = null;
-      result = null;
-      display.textContent = '';
-      return;
-    }
-    
-    // Check for backspace button
-    if (buttonValue === '←') {
-      currentNumber = currentNumber.slice(0, -1);
-      display.textContent = currentNumber;
-      return;
-    }
-    
-    // Check for decimal button
-    if (buttonValue === '.') {
-      if (currentNumber.includes('.')) {
-        return;
-      }
-      
-      if (currentNumber === '') {
-        currentNumber += '0.';
+    if (isNumber) {
+      if (operator === null) {
+        if (operand1 === null) {
+          operand1 = buttonValue;
+        } else {
+          operand1 += buttonValue;
+        }
+        display.innerText = operand1;
       } else {
-        currentNumber += '.';
+        if (operand2 === null) {
+          operand2 = buttonValue;
+        } else {
+          operand2 += buttonValue;
+        }
+        display.innerText = operand2;
       }
-      
-      display.textContent = currentNumber;
-      return;
-    }
-    
-    // Check for operator buttons
-    if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') {
-      if (firstNumber === null) {
-        firstNumber = parseFloat(currentNumber);
-        currentNumber = '';
-        operator = buttonValue;
-        display.textContent = '';
-      } else {
-        const secondNumber = parseFloat(currentNumber);
-        currentNumber = '';
-        const result = calculate(firstNumber, secondNumber, operator);
-        firstNumber = result;
-        operator = buttonValue;
-        display.textContent = '';
-      }
-      
-      return;
-    }
-    
-    // Check for equals button
-    if (buttonValue === '=') {
-      if (firstNumber === null) {
-        return;
-      }
-      
-      const secondNumber = parseFloat(currentNumber);
-      currentNumber = '';
-      result = calculate(firstNumber, secondNumber, operator);
-      firstNumber = null;
+    } else if (buttonValue === "C") {
+      operand1 = null;
       operator = null;
-      display.textContent = result;
-      
-      return;
+      operand2 = null;
+      display.innerText = "";
+    } else if (buttonValue === "=") {
+      if (operator !== null && operand1 !== null && operand2 !== null) {
+        const result = eval(`${operand1} ${operator} ${operand2}`);
+        operand1 = result.toString();
+        operator = null;
+        operand2 = null;
+        display.innerText = operand1;
+      }
+    } else {
+      operator = buttonValue;
     }
-    
-    // If none of the above conditions are met, add the digit to the current number
-    currentNumber += buttonValue;
-    display.textContent = currentNumber;
   });
 });
-
-// Helper function to perform calculations
-function calculate(num1, num2, operator) {
-  switch (operator) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    case '/':
-      return num1 / num2;
-  }
-}
